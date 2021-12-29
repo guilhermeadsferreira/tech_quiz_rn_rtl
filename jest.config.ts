8febc70df2e5544512d3d912d45e4934be21e023
Guilherme Augusto <guilherme@MacBook-Pro-de-Guilherme.local>
2021-12-29 18:36:42 -0300

    jest adjustments

--- jest.config.ts
+++ jest.config.ts
@@ -177,9 +177,8 @@ export default {
   //   "\\.pnp\\.[^\\/]+$"
   // ],
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)",
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
-
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
